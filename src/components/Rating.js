a6b43a4e26cc766b84cea2a929a9a6432b7740d1
Djibril Bathily <bathilydbl@gmail.com>
2022-06-16 14:40:57 +0200

    code refactor

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,12 +3,6 @@ import star from "../assets/icons/star.svg";
 import "../styles/Rating.css";
 
 function Rating(props) {
-  var average;
-  if (Math.round(props.average) > 0 && Math.round(props.average) < 5) {
-    average = Math.round(props.average);
-  } else {
-    average = 0;
-  }
   return (
     <div className="Rating">
       <span>
